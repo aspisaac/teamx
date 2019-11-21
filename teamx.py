4788c23b25853508e75e530070a6fe08f0a130cb
aspisaac <57911004+aspisaac@users.noreply.github.com>
2019-11-21 11:29:57 -0500

    Update teamx.py

--- teamx.py
+++ teamx.py
@@ -63,6 +63,62 @@ def drawRainbow():
         x.forward(5)
         x.right(2)
 
-
+def drawBus():#ISAAC
+  x.fillcolor("yellow")
+  x.begin_fill()
+  for i in range(2):
+    x.forward(150)
+    x.left(90)
+    x.forward(70)
+    x.left(90)
+  x.end_fill()
+  x.fd(20)
+  x.fillcolor("silver")
+  x.begin_fill()
+  for i in range(360):
+    x.forward(.3)
+    x.right(1)
+  x.end_fill()
+  x.forward(130)
+  x.begin_fill()
+  for i in range(360):
+    x.forward(.3)
+    x.right(1)
+  x.end_fill()
+  x.fillcolor("yellow")
+  x.begin_fill()
+  for i in range(4):
+    x.forward(30)
+    x.left(90)
+  x.end_fill()
+  x.bk(20)
+  x.lt(90)
+  x.fillcolor("darkkhaki")
+  x.begin_fill()
+  for i in range(2):
+    x.fd(50)
+    x.lt(90)
+    x.fd(20)
+    x.lt(90)
+  x.end_fill()
+  x.fd(50)
+  x.lt(90)
+  x.penup()
+  x.fd(40)
+  x.pendown()
+  x.begin_fill()
+  for i in range(4):
+    x.fd(20)
+    x.left(90)
+  x.end_fill()
+  x.penup()
+  x.fd(50)
+  x.pendown()
+  x.begin_fill()
+  for i in range(4):
+    x.fd(20)
+    x.left(90)
+  x.end_fill()
+  x.hideturtle()
 
 update()
